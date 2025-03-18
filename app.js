d84968ef8f40f7041cfa15dc63f2d4dc67052eeb
Bianca <weinberger.bianca28@gmail.com>
2025-03-18 05:09:28 -0300

    update commit

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigo = [];
+const lista = document.getElementById('listaAmigos');
+const inNome = document.getElementById('amigo');
+const resultado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+    const nomeAmigo = inNome.value;
+
+    if (!nomeAmigo) {
+        window.alert('Digite um nome válido!');
+        return;
+    }
+    if (listaAmigo.includes(nomeAmigo)) {
+        window.alert('Amigo já adicionado!');
+        return;
+    }
+    listaAmigo.push(nomeAmigo);
+    listaAtualizada();
+    inNome.value = "";
+}
+
+function listaAtualizada() {
+    lista.innerHTML = "";
+    listaAmigo.forEach((amigo, index) => {
+        const item = document.createElement('li'); 
+        item.textContent = amigo;
+        const btRemover = document.createElement('button');
+        btRemover.textContent = 'Remover';
+        btRemover.classList.add('button-remove');
+        btRemover.onclick = () => removerAmigo(index); 
+        item.appendChild(btRemover);
+        lista.appendChild(item);
+    });
+}
+
+function removerAmigo(index) {
+    listaAmigo.splice(index, 1);
+    listaAtualizada();
+}
+
+function sortearAmigo() {
+    if (listaAmigo.length === 0) {
+        window.alert('Adicione um amigo para o sorteio!');
+        return;
+    }
+    const amigoSorteado = listaAmigo[Math.floor(Math.random() * listaAmigo.length)];
+    resultado.innerHTML = `<li>O amigo sorteado é: <strong>${amigoSorteado}</strong></li>`; 
+}
+
+function limparLista() {
+    listaAmigo = [];
+    listaAtualizada();
+    resultado.innerHTML = '';
+}
